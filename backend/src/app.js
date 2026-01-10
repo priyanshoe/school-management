b27cd7864a5d49857bb8fa124d4d3114b97542d5
priyanshoe <priyanshurahate@gmail.com>
2026-01-10 23:51:53 +0530

    hashed password, saved cookie

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-
 app.use(express.json());  
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 app.get("/",(req,res) =>{
     res.send("Helooo")
 })
--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,10 +1,6 @@
 const pool = require('../db/db')
-
-function registerTeacher(req,res){
-
-    res.send("Teahcer user regestered")
-    
-}
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 async function getTeachers(req,res){
      try {
@@ -32,42 +28,38 @@ async function registerTeacher(req,res){
         const { name, email, photo, phone, address, dob, blood_group, bio, password } = req.body
         const [user] =  await pool.query("SELECT * FROM teachers WHERE email = ?",[email])
         if(user.length>0) return res.status(409).json({ message: "Teacher already exist"});
+        const hashPassword = await bcrypt.hash(password, 10)
         const [created] = await pool.query("INSERT INTO teachers (name, email,photo,phone,address,dob,blood_group,bio,password) VALUES (?,?,?,?,?,?,?,?,?)",
-                            [name, email,photo,phone,address,dob,blood_group,bio,password])
+                            [name, email,photo,phone,address,dob,blood_group,bio,hashPassword])
+        const token = jwt.sign({id:created.insertId}, 'secret_code')
+        res.cookie('token',token);        
         return res.status(200).json({ message: "SignIn Success", data: created.insertId });
     } catch (err){
         return res.status(500).json({ message: "SignIn failed", err });
     }
 }
-
+async function check() {
+    const token = jwt.sign({id:23}, 'secret_code')
+    const verify = jwt.verify(token, 'secret_code')
+        console.log(verify.id);
+}
+check()
 
 async function loginTeacher(req,res){
     try{
         const {email , password} = req.body;
         const [user] =  await pool.query("SELECT * FROM teachers WHERE email = ?",[email])
-        if(user.length === 0){
-            return res.status(409).json({message:"Wrong Credential"});
-        }
-        if(user[0].password === password){
-            return res.status(200).json({ message: "LogIn Success", data: user });
-        }
-        else{
-            return res.status(409).json({message:"Wrong Credential"});
-        }
+        if(user.length === 0) return res.status(409).json({message:"Wrong Credential"});
+        const hashPassword = await bcrypt.compare(password, user[0].password)
+        if(!hashPassword) return res.status(409).json({message:"Wrong Credential"});
+        const token = jwt.sign({id:user[0].id}, 'secret_code')
+        res.cookie('token',token);
+        return res.status(200).json({ message: "LogIn Success", data: user });
     } catch(err){
         return res.status(500).json({ message: "SignIn failed", error: err });
     }
 }
-// setTeacher(
-//   'Daniel Brooks',
-//   'daniel.brooks@school.com',
-//   'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg',
-//   '5559876543',
-//   '89 Pine Road, Denver, USA',
-//   '1986-10-08',
-//   'B+',
-//   'Economics teacher focusing on real-world applications and financial literacy'
-// )
+
 module.exports = {
     registerTeacher,
     getTeachers,
